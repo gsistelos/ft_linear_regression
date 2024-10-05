b9b6af2543f99deeb7be444de549cc0e3a97c778
gsistelos <gabrielsistelos@gmail.com>
2024-10-05 19:44:52 -0300

    refactor with comments and some changes

--- main.py
+++ main.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
 
-from pandas import read_csv
+import pandas as pd
+import math_utils as mu
 
-from math_utils import normalize_dataframe, calculate_mean, calculate_slope, calculate_intercept, linear_function
-from plot_utils import plot_linear_function
 
 if __name__ == "__main__":
-    df = read_csv('data.csv')
+    df = pd.read_csv('data.csv')
 
-    df = normalize_dataframe(df)
+    df = mu.normalize_dataframe(df)
     n = len(df)
 
-    km_mean = calculate_mean(df['km'], n)
-    price_mean = calculate_mean(df['price'], n)
+    x = df['km']
+    y = df['price']
 
-    slope = calculate_slope(df, n, km_mean, price_mean)
-    intercept = calculate_intercept(km_mean, price_mean, slope)
+    x_mean = mu.calculate_mean(x, n)
+    y_mean = mu.calculate_mean(y, n)
 
-    x_values = []
-    y_values = []
+    slope = mu.calculate_slope(x, y, x_mean, y_mean, n)
+    intercept = mu.calculate_intercept(x_mean, y_mean, slope)
 
-    for i in range(100):
-        x = df['km'].min() + (i / 99) * \
-            (df['km'].max() - df['km'].min())
-        x_values.append(x)
-        y_values.append(linear_function(x, slope, intercept))
-
-    plot_linear_function(df, x_values, y_values)
+    print(slope, intercept)
--- math_utils.py
+++ math_utils.py
@@ -1,44 +1,67 @@
 from pandas import DataFrame
+from typing import List
 
 
 def normalize_dataframe(df: DataFrame) -> DataFrame:
     """
-    Perform Min-Max normalization to scale values from 0 to 1.
+    Perform Z-score normalization to standardize the values
 
-    Normalization helps improve the performance
-    of machine learning algorithms by ensuring that
-    features on different scales do not bias the results
+    Z-score normalization centers the data around the mean (0)
+    and scales it by the standard deviation (1)
 
-    Normalized value = (x - min) / (max - min)
+    Normalized value = (x - mean) / std
     """
-    return (df - df.min()) / (df.max() - df.min())
+    return (df - df.mean()) / df.std()
 
 
-def calculate_mean(data, n):
+def calculate_mean(x: List[float], n: int) -> float:
+    """
+    Calculate the mean of a list of numbers
+
+    Mean = sum(x) / n
+    """
     total_sum = 0
 
-    for value in data:
+    for value in x:
         total_sum += value
 
     mean = total_sum / n
     return mean
 
 
-def calculate_slope(df, n, km_mean, price_mean):
+def calculate_slope(
+    x: List[float], y: List[float],
+    x_mean: float, y_mean: float,
+    n: int,
+) -> float:
+    """
+    Calculate the slope of the regression line
+
+    The slope quantifies how much y changes for a unit change in x
+
+    Slope = sum((x - x_mean) * (y - y_mean)) / sum((x - x_mean)^2)
+    """
     numerator = 0
     denominator = 0
 
-    for index in range(n):
-        numerator += (df['km'][index] - km_mean) * \
-            (df['price'][index] - price_mean)
-        denominator += (df['km'][index] - km_mean) ** 2
+    for i in range(n):
+        x_deviation = (x[i] - x_mean)
+        y_deviation = (y[i] - y_mean)
+
+        numerator += x_deviation * y_deviation
+        denominator += x_deviation ** 2
 
     return numerator / denominator
 
 
-def calculate_intercept(km_mean, price_mean, slope):
-    return price_mean - slope * km_mean
+def calculate_intercept(
+    x_mean: float, y_mean: float, slope: float,
+) -> float:
+    """
+    Calculate the intercept of the regression line
 
+    Intercept is the value of y when x is 0
 
-def linear_function(x, slope, intercept):
-    return slope * x + intercept
+    Intercept = y_mean - slope * x_mean
+    """
+    return y_mean - slope * x_mean
